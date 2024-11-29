b1495a4b3f7b78233e5221a38443bbafd3ce0f2e
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-11-29 09:00:26 +0530

    Add files via upload

---
+++ palindromlistornot.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+
+    public:
+    int val;
+    Node* prev;
+    Node* next;
+
+    Node(int data){
+        val=data;
+        prev=NULL;
+        next=NULL;
+    }
+
+};
+
+class Doublyll{
+
+    public:
+    Node* head;
+    Node* tail;
+
+    Doublyll(){
+        head=NULL;
+        tail=NULL;
+    }
+
+    void display(){
+
+        Node* temp = head;
+        while(temp!=NULL){
+            cout<<temp->val<<"<->";
+            temp=temp->next;
+        }cout<<endl;
+    }
+
+    void insertAtStart(int val){
+
+        Node* new_node = new Node(val);
+        if(head==NULL){
+            head = new_node;
+            tail = new_node;
+            return;
+        }
+
+        new_node->next = head;
+        head->prev = new_node;
+        head = new_node;
+        return;
+    }
+
+    void insertAtEnd(int val){
+
+        Node* new_node = new Node(val);  
+        if(tail==NULL){
+            head = new_node;
+            tail = new_node;
+            return;
+        }
+
+        tail->next = new_node;
+        new_node->prev = tail;
+        tail = new_node;
+        return;
+    }
+
+    void insertAtPosition(int val , int k){
+
+        Node* temp = head;
+        int count = 1;
+        while(count<k-1){
+            temp = temp->next;
+            count++;
+        }
+
+        //temp will be pointing to the node at (k-1)th position
+        Node* new_node = new Node(val);  
+        new_node->next = temp->next;
+        temp->next = new_node;
+
+        new_node->prev = temp;
+        new_node->next->prev = new_node;
+        return;
+        }
+
+};
+
+bool isPalindroneDll(Node* head , Node* tail){
+
+    while(head!=tail && tail!=head->prev){
+        if(head->val!=tail->val){
+            return false;
+        }
+        head=head->next;
+        tail=tail->prev;
+    }
+    return true;
+}
+
+int main(){
+
+    Doublyll dll;
+    dll.insertAtStart(3);
+    dll.insertAtStart(2);
+    dll.insertAtStart(1);
+    dll.display();
+    dll.insertAtEnd(1);
+    dll.display();
+    dll.insertAtPosition(2,4);
+    dll.display();
+    cout<<isPalindroneDll(dll.head,dll.tail)<<endl;
+
+}
